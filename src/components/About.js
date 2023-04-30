3372527579e4f50c7f17225bb30f8f1872430f77
Thomas Wallingford <tbwallingford@gmail.com>
2023-04-30 13:34:25 -0600

    Complete lab with removed testing p tag

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,6 @@ function About(props) {
     return (
       <div id="about">
         <h2>About Me</h2>
-        <p>No P</p>
         <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
         {/* add your <Links /> component here */}
       </div>
